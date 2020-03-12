fe0cff9941d47ad5d16bbd7821b0860e05613579
arzyu <arzyu@live.cn>
2020-03-12 17:12:24 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# npm-unpublish
+
+Another way to unpublish your npm packages.
+
+## Usage
+
+```shell
+npx npm-unpublish <pkg-name> [reason]
+
+# examples
+npx npm-unpublish my-pkg
+npx npm-unpublish my-pkg "Deprecated, please do not use it!"
+```
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+import shell from "shelljs";
+
+const fail = chalk.red;
+const success = chalk.green;
+
+const args = process.argv;
+
+if (args.length < 3) {
+  console.log(fail("Usage:\n  npx npm-unpublish <pkg-name> [reason]\n"));
+  process.exit(1);
+}
+
+const pkgName = args[2];
+const reason = args[3] || "Deprecated, please do not use it!";
+
+console.log(`💩　Unpublishing ${pkgName}...\n`);
+
+const cmdList = [
+  `npm deprecate ${pkgName} "${reason}"`,
+  `npm owner add npm ${pkgName}`,
+  `npm owner rm $(npm whoami) ${pkgName}`
+];
+
+const run = (cmd: string) => new Promise((resolve, reject) => {
+  shell.exec(cmd, { silent: true }, (code, stdout, stderr) => {
+    if (!code) resolve(stdout);
+    else reject(stderr);
+  });
+});
+
+(async () => {
+  for (let i = 0; i < cmdList.length; i++) {
+    await run(cmdList[i]);
+  }
+  console.log(success("🎉　Done."));
+})()
+  .then(result => console.log(success(result)))
+  .catch(error => console.log(fail(error)));
